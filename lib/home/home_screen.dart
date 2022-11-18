edaa415a871b057a7979acbce69766cf2b7f1d1c
Ashmaawy <mohamedashmawy918@gmail.com>
2022-11-18 23:31:57 +0200

    initial ashmawi commit2

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -13,107 +13,140 @@ class HomeScreen extends StatelessWidget {
 
   final TextEditingController timeWaitingController = TextEditingController();
   final TextEditingController numberOfClientsController =
-      TextEditingController();
+  TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     var formKey = GlobalKey<FormState>();
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.greenAccent,
+        backgroundColor: Colors.white,
         extendBodyBehindAppBar: true,
         body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
           child: Form(
             key: formKey,
             child: Padding(
-              padding: const EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(5.0),
               child: Column(
                 children: [
-
-                  const DefaultHeadLine(
-                    text: 'D/D/1/K-1',
-                  ),
                   const SizedBox(
-                    height: 10,
+                    height: 30,
                   ),
-                  DefaultTextForm(
-                    onChange: (v) {},
-                    validator: (value) {
-                      for (int i = 0; i < value!.length; i++) {
-                        if (value[i] != '0' &&
-                            value[i] != '1' &&
-                            value[i] != '2' &&
-                            value[i] != '3' &&
-                            value[i] != '4' &&
-                            value[i] != '5' &&
-                            value[i] != '6' &&
-                            value[i] != '7' &&
-                            value[i] != '8' &&
-                            value[i] != '9') {
-                          return 'it must be integer';
-                        }
-                      }
-                      if (value.isEmpty) return 'it required';
-                      return null;
-                    },
-                    textEditingController: capacityTimeController,
-                    suffix: Text(
-                      'K-1',
-                      style: Theme.of(context).textTheme.caption,
+                  Container(
+                    height: 35,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    label: 'Capacity',
+                    child: const Center(
+                      child: Text(
+                        'D/D/1/K-1',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
                   ),
-                  DefaultTextForm(
-                    onChange: (v) {},
-                    validator: (value) {
-                      for (int i = 0; i < value!.length; i++) {
-                        if (value[i] != '0' &&
-                            value[i] != '1' &&
-                            value[i] != '2' &&
-                            value[i] != '3' &&
-                            value[i] != '4' &&
-                            value[i] != '5' &&
-                            value[i] != '6' &&
-                            value[i] != '7' &&
-                            value[i] != '8' &&
-                            value[i] != '9' &&
-                            value[i] != '.') return 'it must be valid';
-                      }
-                      if (value.isEmpty) return 'it required';
-                      return null;
-                    },
-                    textEditingController: arrivalTimeController,
-                    suffix: Text(
-                      '1/λ',
-                      style: Theme.of(context).textTheme.caption,
-                    ),
-                    label: 'Arrival time ',
-                  ),
-                  DefaultTextForm(
-                    onChange: (v) {},
-                    validator: (value) {
-                      for (int i = 0; i < value!.length; i++) {
-                        if (value[i] != '0' &&
-                            value[i] != '1' &&
-                            value[i] != '2' &&
-                            value[i] != '3' &&
-                            value[i] != '4' &&
-                            value[i] != '5' &&
-                            value[i] != '6' &&
-                            value[i] != '7' &&
-                            value[i] != '8' &&
-                            value[i] != '9' &&
-                            value[i] != '.') return 'it must be valid';
-                      }
-                      if (value.isEmpty) return 'it required';
-                      return null;
-                    },
-                    textEditingController: serviceTimeController,
-                    suffix: Text(
-                      '1/µ',
-                      style: Theme.of(context).textTheme.caption,
-                    ),
-                    label: 'Service time ',
+                  dashLines(),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: DefaultTextForm(
+                          onChange: (v){
+
+                          },
+                          validator: (value) {
+                            for (int i = 0; i < value!.length; i++) {
+                              if (value[i] != '0' &&
+                                  value[i] != '1' &&
+                                  value[i] != '2' &&
+                                  value[i] != '3' &&
+                                  value[i] != '4' &&
+                                  value[i] != '5' &&
+                                  value[i] != '6' &&
+                                  value[i] != '7' &&
+                                  value[i] != '8' &&
+                                  value[i] != '9' &&
+                                  value[i] != '.') return 'it must be valid';
+                            }
+                            if (value.isEmpty) return 'it required';
+                            return null;
+                          },
+                          textEditingController: arrivalTimeController,
+                          suffix: Text(
+                            '1/λ',
+                            style: Theme.of(context).textTheme.caption,
+                          ),
+                          label: 'Arrival time ',
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: DefaultTextForm(
+                            onChange: (v){
+
+                            },
+                            validator: (value) {
+                              for (int i = 0; i < value!.length; i++) {
+                                if (value[i] != '0' &&
+                                    value[i] != '1' &&
+                                    value[i] != '2' &&
+                                    value[i] != '3' &&
+                                    value[i] != '4' &&
+                                    value[i] != '5' &&
+                                    value[i] != '6' &&
+                                    value[i] != '7' &&
+                                    value[i] != '8' &&
+                                    value[i] != '9') {
+                                  return 'it must be integer';
+                                }
+                              }
+                              if (value.isEmpty) return 'it required';
+                              return null;
+                            },
+                            textEditingController: capacityTimeController,
+                            suffix: Text(
+                              'K-1',
+                              style: Theme.of(context).textTheme.caption,
+                            ),
+                            label: 'Capacity',
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: DefaultTextForm(
+                          onChange: (v){
+
+                          },
+                          validator: (value) {
+                            for (int i = 0; i < value!.length; i++) {
+                              if (value[i] != '0' &&
+                                  value[i] != '1' &&
+                                  value[i] != '2' &&
+                                  value[i] != '3' &&
+                                  value[i] != '4' &&
+                                  value[i] != '5' &&
+                                  value[i] != '6' &&
+                                  value[i] != '7' &&
+                                  value[i] != '8' &&
+                                  value[i] != '9' &&
+                                  value[i] != '.') return 'it must be valid';
+                            }
+                            if (value.isEmpty) return 'it required';
+                            return null;
+                          },
+                          textEditingController: serviceTimeController,
+                          suffix: Text(
+                            '1/µ',
+                            style: Theme.of(context).textTheme.caption,
+                          ),
+                          label: 'Service time ',
+                        ),
+                      ),
+                    ],
                   ),
                   const DefaultHeadLine(
                     text: 'Calculate  n(t) = ?',
@@ -121,8 +154,8 @@ class HomeScreen extends StatelessWidget {
                   BlocBuilder<LogicCubit, LogicState>(
                     builder: (context, state) {
                       var cubit = context.read<LogicCubit>();
-                      return DefaultTextForm(
-                        onChange: (v) {
+                      return  DefaultTextForm(
+                        onChange: (v){
                           cubit.changeN(v);
                         },
                         validator: (value) {
@@ -147,8 +180,7 @@ class HomeScreen extends StatelessWidget {
                           't  ',
                           style: Theme.of(context).textTheme.titleLarge,
                         ),
-                        label:
-                            'Number of clients in the system at specific time',
+                        label: 'Number of clients in the system at specific time',
                       );
                     },
                   ),
@@ -180,11 +212,11 @@ class HomeScreen extends StatelessWidget {
                     builder: (context, state) {
                       var cubit = context.read<LogicCubit>();
                       return DefaultTextForm(
-                        onChange: (v) {
+                        onChange: (v){
                           cubit.changeW(v);
                         },
-                        validator: (value) {
 
+                        validator: (value) {
                           for (int i = 0; i < value!.length; i++) {
                             if (value[i] != '0' &&
                                 value[i] != '1' &&
@@ -338,25 +370,24 @@ class HomeScreen extends StatelessWidget {
             builder: (context, state) {
               var cubit = context.read<LogicCubit>();
               return  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        if (cubit.resultNumOfClients != null)
-                          Text(
-                            'n(${cubit.calcTime ?? 't'})  = ${cubit.resultNumOfClients}',
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ),
-                        if (cubit.resultNumOfClients != null)
-                          const SizedBox(
-                            height: 20,
-                          ),
-                        if (cubit.resultTimeClintWillTake != null)
-                          Text(
-                            'Wq(${cubit.calcNumber ?? 'n'}) = ${cubit.resultTimeClintWillTake}',
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ),
-                      ],
-                    )
-                 ;
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (cubit.resultNumOfClients != null)
+                    Text(
+                      'n(${cubit.calcTime ?? 't'})  = ${cubit.resultNumOfClients}',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  if (cubit.resultNumOfClients != null)
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  if (cubit.resultTimeClintWillTake != null)
+                    Text(
+                      'Wq(${cubit.calcNumber ?? 'n'}) = ${cubit.resultTimeClintWillTake}',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                ],
+              );
             },
           ),
         ),
